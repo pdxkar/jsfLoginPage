e7c5593ea601cc4fbdfa726769446f8f6ad0a0a6
KatherineRoot <kroot@catalystitservices.com>
2014-06-17 15:17:19 -0700

    working ajax functionality

--- src/main/java/com/tutorialspoint/test/UserData.java
+++ src/main/java/com/tutorialspoint/test/UserData.java
@@ -9,41 +9,17 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class UserData implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	private String name;
-	private String password;
+   private String name;
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
 
-	private String response2;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getResponse2() {
-		return response2;
-	}
-
-	public void setResponse2(String response2) {
-		this.response2 = response2;
-	}
-
-	public String login() {
-		System.out.println("in login method");
-	//	return "response";
-		setResponse2(getName());
-		return response2;
-	}
+   public String getWelcomeMessage(){
+      return "Hello " + name;
+   }
 }
