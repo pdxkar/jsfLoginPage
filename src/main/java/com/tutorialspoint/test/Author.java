b492b1356aede5342650b301a16b4558aa2bd4da
KatherineRoot <kroot@catalystitservices.com>
2014-06-18 12:21:12 -0700

    ajax functionality works

---
+++ src/main/java/com/tutorialspoint/test/Author.java
@@ -0,0 +1,18 @@
+package com.tutorialspoint.test;
+
+public class Author {
+   int id;
+   String name;
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public int getId() {
+      return id;
+   }
+   public void setId(int id) {
+      this.id = id;
+   }
+}
--- src/main/java/com/tutorialspoint/test/UserData.java
+++ src/main/java/com/tutorialspoint/test/UserData.java
@@ -1,9 +1,17 @@
 package com.tutorialspoint.test;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ComponentSystemEvent;
 
 @ManagedBean(name = "userData", eager = true)
 @SessionScoped
@@ -11,15 +19,43 @@ public class UserData implements Serializable {
 
    private static final long serialVersionUID = 1L;
 
-   private String name;
-   public String getName() {
-      return name;
-   }
-   public void setName(String name) {
-      this.name = name;
+   public List<Author> getAuthors(){
+      ResultSet rs = null;
+      PreparedStatement pst = null;
+      Connection con = getConnection();
+      String stm = "Select * from authors";
+      List<Author> records = new ArrayList<Author>();
+      try {   
+         pst = con.prepareStatement(stm);
+         pst.execute();
+         rs = pst.getResultSet();
+
+         while(rs.next()){
+            Author author = new Author();
+            author.setId(rs.getInt(1));
+            author.setName(rs.getString(2));
+            records.add(author);				
+         }
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      return records;
    }
 
-   public String getWelcomeMessage(){
-      return "Hello " + name;
+   public Connection getConnection(){
+      Connection con = null;
+
+      String url = "jdbc:postgresql://localhost/testdb";
+      String user = "postgres";
+      String password = "root";
+      try {
+         con = DriverManager.getConnection(url, user, password);
+         System.out.println("Connection completed.");
+      } catch (SQLException ex) {
+         System.out.println(ex.getMessage());
+      }
+      finally{
+      }
+      return con;
    }
 }
