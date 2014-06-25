af67cf25a93fd113dc53646d4db85c0d755886ea
KatherineRoot <kroot@catalystitservices.com>
2014-06-25 09:58:09 -0700

    internationalization works

--- src/main/java/com/tutorialspoint/test/UserData.java
+++ src/main/java/com/tutorialspoint/test/UserData.java
@@ -1,49 +1,50 @@
 package com.tutorialspoint.test;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean(name = "userData", eager = true)
 @SessionScoped
 public class UserData implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
+   private String locale;
 
-	private String name;
-	private String password;
+   private static Map<String,Object> countries;
+   
+   static{
+      countries = new LinkedHashMap<String,Object>();
+      countries.put("English", Locale.ENGLISH);
+      countries.put("French", Locale.FRENCH);
+   }
 
-	private String response2;
+   public Map<String, Object> getCountries() {
+      return countries;
+   }
 
-	public String getName() {
-		return name;
-	}
+   public String getLocale() {
+      return locale;
+   }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+   public void setLocale(String locale) {
+      this.locale = locale;
+   }
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getResponse2() {
-		return response2;
-	}
-
-	public void setResponse2(String response2) {
-		this.response2 = response2;
-	}
-
-	public String login() {
-		System.out.println("in login method");
-	//	return "response";
-		setResponse2(getName());
-		return response2;
-	}
+   //value change event listener
+   public void localeChanged(ValueChangeEvent e){
+      String newLocaleValue = e.getNewValue().toString();
+      for (Map.Entry<String, Object> entry : countries.entrySet()) {
+         if(entry.getValue().toString().equals(newLocaleValue)){
+            FacesContext.getCurrentInstance()
+               .getViewRoot().setLocale((Locale)entry.getValue());         
+         }
+      }
+   }
 }
